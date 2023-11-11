1dec68de7ca39e0c4fe23b733faa7cfe690774f2
Maria Eduarda <142428634+iduarda@users.noreply.github.com>
2023-11-10 21:29:57 -0300

    adicionando login, cadastrar e banco de dados para o usuario

---
+++ estoque-online/sistema.php
@@ -0,0 +1,48 @@
+<?php
+
+    session_start();
+    // print_r($_SESSION);
+
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: login.php');
+    }
+    $logado = $_SESSION['email'];
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Sistema</title>
+    <style>
+        body{
+            background-image: linear-gradient(45deg, #3ed6ae, #2edbf2);
+            color: #fff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Easy Estoque</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="d-flex">
+        <a href="sair.php" class="btn btn-danger me-5">Sair</a>
+    </div>
+    </nav>
+    <br>
+    <?php
+
+        echo "<h1>Bem vindo <u>$logado</u></h1>";
+
+    ?>
+</body>
+</html>
